8483c0f9d7fd994301523ec31222458e4f9fc905
Sebastian Boutin Blomfield <ukuleleseb@gmail.com>
2021-03-18 13:47:12 +0100

    tile group system

---
+++ TileMapLookup.gd
@@ -0,0 +1,22 @@
+extends TileMap
+
+
+# Declare member variables here. Examples:
+export var tile_groups = {}
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func get_group(pos):
+	var coords = get_cell_autotile_coord(pos.x, pos.y)
+	print_debug(coords)
+	for key in tile_groups:
+		for v in tile_groups[key]:
+			if v == coords:
+				return key
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- WorldMap.gd
+++ WorldMap.gd
@@ -9,13 +9,25 @@ func _ready():
 	for x in range(width):
 		entities.append([])
 		entities[x].resize(height)
-	tile_map = get_node("TileMap") as TileMap
+	tile_map = get_node("TileMap")
+	var base = get_node("Base")
+	add_entity(base, base.position)
 		
 func add_entity(entity, pos):
 	var tile_pos = tile_map.world_to_map(pos)
-	if tile_pos.x < 0 || tile_pos.x > entities.size() || tile_pos.y < 0 || tile_pos.y > entities[tile_pos.x].size() || entities[tile_pos.x][tile_pos.y]: return
+	if tile_pos.x < 0 || tile_pos.x > entities.size() - 1 || tile_pos.y < 0 || tile_pos.y > entities[tile_pos.x].size() - 1 || entities[tile_pos.x][tile_pos.y]: return
+	
+	var group = tile_map.get_group(tile_pos)
+	if group == 'road' || group == 'water' || group == 'tree': return
+	
 	add_child(entity)
-	entities[tile_pos.x][tile_pos.y] = entity
+	var tilemap_entity = entity as TileMapEntity
+	if tilemap_entity:
+		for x in range(tilemap_entity.width):
+			for y in range(tilemap_entity.height):
+				entities[tile_pos.x + x][tile_pos.y + y] = entity
+	else: entities[tile_pos.x][tile_pos.y] = entity
+	
 	entity.position = Vector2(tile_pos.x * tile_map.cell_size.x, tile_pos.y * tile_map.cell_size.y)
 	entity.z_index = tile_pos.y
 	
